df5654cf2e8822ce18e8c028c5c849e07ff1c76a
difuster <iam0kay@yandex.ru>
2021-10-25 22:18:02 +0600

    update brain-even andeven-game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
-import startGame from '../src/games/even-game.js';
+import {
+  startGame, rule, question, func,
+} from '../src/games/even-game.js';
 
-startGame();
+startGame(rule, question, func);
--- src/games/even-game.js
+++ src/games/even-game.js
@@ -1,42 +1,18 @@
-import readlineSync from 'readline-sync';
+import { getRandomNum, startGame } from '../index.js';
 
-const startGame = () => {
-  let q = 1;
-  let userName = '';
-  let result = 0;
-  console.log('Welcome to the Brain Games!');
-  userName = readlineSync.question('May I have your name? ');
-  console.log(`Hi ${userName} !`);
-  const evenGame = () => {
-    const num = Math.floor(Math.random() * 98) + 1;
-    let rightAnswer;
-    if (num % 2 === 0) {
-      rightAnswer = 'yes';
-    } else {
-      rightAnswer = 'no';
-    }
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-    const answer = readlineSync.question(`Question: ${num} `);
-    console.log(`Your answer: ${answer}`);
-    if ((num % 2 === 0 && answer === 'yes') || (num % 2 !== 0 && answer === 'no')) {
-      console.log('Correct!');
-      result += result;
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'`);
-      result = -1;
-    }
-    if (q === 3) {
-      console.log(`Congratulations, ${userName}!`);
-    }
-    if (q < 3) {
-      console.log(`Let's try again, ${userName}!`);
-    }
-  };
-
-  while (q < 4 && result >= 0) {
-    evenGame();
-    q += 1;
+const number = getRandomNum();
+const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
+const question = `Question: ${number} `;
+const func = () => {
+  let answer = '';
+  if (number % 2 === 0) {
+    answer = 'yes';
+  } else {
+    answer = 'no';
   }
+  return answer;
 };
 
-export default startGame;
+export {
+  startGame, rule, question, func,
+};

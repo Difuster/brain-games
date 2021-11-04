90970608e8e1296bca2792099bbd91781b8be2b9
difuster <iam0kay@yandex.ru>
2021-11-04 12:02:44 +0600

    updated even-game, prime-game

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -5,7 +5,7 @@ const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 /* get right answer */
 const getQuestionAndCorrectAnswer = () => {
-  const num = getRandomNum(99)+1;
+  const num = getRandomNum(99) + 1;
   const question = num;
   let correctAnswer = '';
   let i = 2;
